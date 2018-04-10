bedc721b479ae3874189aec02014132051bfbbd8
tingqianlin <diyidea921@gmail.com>
2018-04-10 16:32:10 +0800

    Make nav responsive.

--- resources/js/script.js
+++ resources/js/script.js
@@ -62,4 +62,18 @@ $(document).ready(function() {
     $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass("animated pulse")
     }, { offset: '50%' });
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round')
+            icon.removeClass('ion-navicon-round')
+        } else {
+            icon.addClass('ion-navicon-round')
+            icon.removeClass('ion-close-round')
+        }
+    });
 });
